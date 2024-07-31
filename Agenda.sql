ce1f5ef272a3ce99e9aec32df01ed9a3e1e21f4a
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-07-31 11:28:01 -0300

    Add files via upload
    Add Agenda SQL - Aula intro

---
+++ Agenda.sql
@@ -0,0 +1,17 @@
+USE agenda;
+SELECT * FROM agenda;
+
+/*
+INSERT INTO agenda(
+nomeCompleto, email, numeroCelular, emailComercial, numeroComercial, dataNascimento, endereco, numero, complemento, cep, bairro, cidade, uf, pais, fusoHorario, descricao
+) VALUE (
+"Ren Wrobleski", "renbiawro@gmail.com", "+5511918746249", "renatabiawro@gmail.com", "", "1984/09/06", "Rua Tito", "54", "3º Andar", "99999-999", "Lapa", "São Paulo", "SP", "BRA", "-03:00", ""
+ );
+ */
+ /* UPDATE agenda SET
+ dataNascimento = "1984/09/06"
+ WHERE nomeCompleto = "Ren Wrobleski";
+*/
+
+
+ 
